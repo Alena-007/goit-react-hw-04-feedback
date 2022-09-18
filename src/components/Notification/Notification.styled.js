f55c5278da279b4a8ab96438523903d6ad6a600c
Alena-007 <alena.tv@ukr.net>
2022-09-18 23:22:51 +0300

    add css

--- src/components/Notification/Notification.styled.js
+++ src/components/Notification/Notification.styled.js
@@ -1,1 +1,9 @@
+import styled from 'styled-components';
 
+export const NotificationMessage = styled.p`
+  display: inline-block;
+  margin-bottom: ${p => p.theme.space[3]}px;
+  font-weight: ${p => p.theme.fontWeights.bold};
+  color: ${p => p.theme.colors.red};
+  font-size: ${p => p.theme.fontSizes.l};
+`;
--- src/components/Statistics/Statistics.styled.js
+++ src/components/Statistics/Statistics.styled.js
@@ -1,1 +1,12 @@
+import styled from 'styled-components';
 
+export const StatisticsItems = styled.li`
+  padding: ${p => p.theme.space[3]}px;
+  margin-bottom: ${p => p.theme.space[2]}px;
+  background-color: ${p => p.theme.colors.white};
+  font-weight: ${p => p.theme.fontWeights.bold};
+  font-size: ${p => p.theme.fontSizes.m};
+  border: ${p => p.theme.borders.normal};
+  border-radius: ${p => p.theme.radii.sm};
+  box-shadow: inset 0 0 10px 5px rgba(0, 0, 0, 0.3);
+`;
